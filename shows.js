505dde649795821463483e094e841bd32376cf19
Jack-R-L-Parkinson <jack6292@gmail.com>
2026-07-05 21:40:44 +0100

    updates to menu and background

--- shows.js
+++ shows.js
@@ -12,3 +12,11 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";}
